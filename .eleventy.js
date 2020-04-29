5096c979d3458af6785a0425aa90857c0cef3bca
Bryce Wray <bwray@wraytx.com>
2020-04-28 19:43:06 -0500

    Config chgs while testing

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,8 @@ const htmlmin = require('html-minifier')
 const ofotigrid = require('./src/_includes/ofotigrid.js')
 const sanitizeHTML = require('sanitize-html')
 const filters = require('./src/assets/utils/filters.js')
-const lazyImagesPlugin = require('eleventy-plugin-lazyimages')
+// const lazyImagesPlugin = require('eleventy-plugin-lazyimages')
+const pluginLocalRespImg = require('eleventy-plugin-local-respimg')
 const ErrorOverlay = require('eleventy-plugin-error-overlay')
 
 module.exports = function (eleventyConfig) {
@@ -79,7 +80,7 @@ module.exports = function (eleventyConfig) {
   })
   eleventyConfig.setLibrary("md", markdownEngine)
 
-  eleventyConfig.addPlugin(lazyImagesPlugin)
+  // eleventyConfig.addPlugin(lazyImagesPlugin)
 
   eleventyConfig.setBrowserSyncConfig({
     ...eleventyConfig.browserSyncConfig,
@@ -94,7 +95,36 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPlugin(ErrorOverlay)
 
-  eleventyConfig.addShortcode("lazypicture", require("./src/assets/utils/lazy-picture.js"))
+  eleventyConfig.addPlugin(pluginLocalRespImg, {
+    folders: {
+      source: 'src',
+      output: '_site',      
+    },
+    images: {
+      resize: {
+        min: 300,
+        max: 1500,
+        step: 300,
+      },
+      gifToVideo: false,
+      sizes: '100vw',
+      lazy: true,
+      additional: [
+        'images/icons/**/*',
+      ],
+      watch: {
+        src: 'images/**/*'
+      },
+      pngquant: {},
+      mozjpeg: {},
+      svgo: {},
+      gifresize: {},
+      webp: {},
+      gifwebp: {},
+    }
+  })
+
+  // eleventyConfig.addShortcode("lazypicture", require("./src/assets/utils/lazy-picture.js"))
 
   eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
     if( outputPath.endsWith(".html") ) {

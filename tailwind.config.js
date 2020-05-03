85cd41c3e8790f9ea067b4ac621aa48b80b62d2f
Bryce Wray <bwray@wraytx.com>
2020-05-03 14:26:28 -0500

    Fixed, so comments no longer valid

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   purge: {
-    enabled: true, 
-    // normally not necessary but TS 1.4 and Purge don't seem to have their stuff together 
-    // re NODE_ENV=production, even if that's declared in the package.json script -- 2020-05-03, 10:04 AM CDT
     content: [
       './src/**/*.js',
       './src/**/*.11ty.js',

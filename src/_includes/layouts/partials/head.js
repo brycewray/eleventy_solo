a884ad72dc1bd8f1a6266ce6fbee4490180b2f7e
Bryce Wray <bwray@wraytx.com>
2020-05-09 13:51:23 -0500

    Fixing refs to "icons" images

--- src/_includes/layouts/partials/head.js
+++ src/_includes/layouts/partials/head.js
@@ -78,19 +78,19 @@ module.exports = function(eleventyConfig) {
     <!-- Favicon -->
     <link rel="icon" href="/favicon.ico">
     <link rel="shortcut icon" href="/favicon.ico">
-    <link rel="apple-touch-icon" sizes="57x57" href="/images/apple-icon-57x57-57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="/images/apple-icon-60x60-60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="/images/apple-icon-72x72-72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="/images/apple-icon-76x76-76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="/images/apple-icon-114x114-114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="/images/apple-icon-120x120-120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="/images/apple-icon-144x144-144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="/images/apple-icon-152x152-152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-icon-180x180-180.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32-32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="/images/favicon-96x96-96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16-16.png">
-    <meta name="msapplication-TileImage" content="/images/ms-icon-144x144-144.png">
+    <link rel="apple-touch-icon" sizes="57x57" href="/images/icons/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/images/icons/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/images/icons/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/images/icons/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/images/icons/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/images/icons/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/images/icons/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/images/icons/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/icons/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/images/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/images/icons/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/images/icons/favicon-16x16.png">
+    <meta name="msapplication-TileImage" content="/images/ms-icon-icons/144x144.png">
 
     <link rel="stylesheet" href="/css/index.css" type="text/css">
 

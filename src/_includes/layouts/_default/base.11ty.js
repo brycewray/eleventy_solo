17d267d9a6a218b24634c07be7a9103927488f16
Bryce Wray <bwray@wraytx.com>
2020-05-15 15:11:12 -0500

    More config changes for performance

--- src/_includes/layouts/_default/base.11ty.js
+++ src/_includes/layouts/_default/base.11ty.js
@@ -92,8 +92,8 @@ module.exports = function (data) {
   <link rel="icon" type="image/png" sizes="16x16" href="/images/icons/favicon-16x16.png">
   <meta name="msapplication-TileImage" content="/images/ms-icon-icons/144x144.png">
 
-  <link rel="preload" href="/css/index.css" as="style">
-  <link rel="stylesheet" href="/css/index.css" type="text/css">
+  <link rel="preload" as="style" href="/css/index.css" />
+  <link rel="stylesheet" href="/css/index.css" type="text/css" />
   <style>.ieOnly {display: none;}@media all and (-ms-high-contrast: none), (-ms-high-contrast: active) {.ieOnly {display: block;}}</style>
 
   <noscript>
@@ -160,9 +160,8 @@ module.exports = function (data) {
         : ``
         }
       </div>
-    </footer> 
+    </footer>
     <script src="/assets/js/lazysizes.min.js" defer></script>
-    <script src="/assets/js/ls.blur-up.min.js" defer></script>
     <script src="/assets/js/flying-pages.min.js" defer></script>
     <script src="/assets/js/twitterMeta.min.js" defer></script>
   </body>

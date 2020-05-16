8c213f5b0bd046c5ef9ce715de69cc54d9901f02
Bryce Wray <bwray@wraytx.com>
2020-05-15 20:35:52 -0500

    Trying without Flying Pages

--- src/_includes/layouts/_default/base.11ty.js
+++ src/_includes/layouts/_default/base.11ty.js
@@ -165,8 +165,8 @@ module.exports = function (data) {
         }
       </div>
     </footer>
-    <script src="/assets/js/lazysizes.min.js" defer></script>
-    <script src="/assets/js/flying-pages.min.js" defer></script>
+    <script src="/assets/js/lazysizes.min.js" defer></script><!--
+    <script src="/assets/js/flying-pages.min.js" defer></script>-->
     <script src="/assets/js/twitterMeta.min.js" defer></script>
   </body>
 </html>

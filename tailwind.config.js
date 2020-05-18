6a2931f0e2de1da17162325e3cb106b3b41664df
Bryce Wray <bwray@wraytx.com>
2020-05-17 21:19:01 -0500

    Testing config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,10 @@ module.exports = {
   },
   theme: {
     screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
+      'sm': '640px',  // => @media (min-width: 640px) { ... }
+      'md': '768px',  // => @media (min-width: 768px) { ... }
+      'lg': '1024px', // => @media (min-width: 1024px) { ... }
+      'xl': '1280px', // => @media (min-width: 1280px) { ... }
     },
     fontFamily: {
       body: ['Public Sans', 'sans-serif'],

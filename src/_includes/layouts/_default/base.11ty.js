e930de48ab45ddfda23172a1ff14881927fcc0a0
Bryce Wray <bwray@wraytx.com>
2020-06-25 07:15:07 -0500

    lazyload 17.0.0; uncommenting-out Flying Pages

--- src/_includes/layouts/_default/base.11ty.js
+++ src/_includes/layouts/_default/base.11ty.js
@@ -7,15 +7,12 @@ module.exports = function (data) {
     ${this.siteHeader(data)}
     ${data.content}
     ${this.siteFooter(data)}
-    <!-- <script src="/assets/js/flying-pages.min.js" defer></script> -->
+    <script src="/assets/js/flying-pages.min.js" defer></script>
     <script src="/assets/js/twitterMeta.min.js" defer></script>
     <script src="/assets/js/lazyload.min.js"></script>
     <script>
       var lazyLoadInstance = new LazyLoad({
-        elements_selector: ".lazy",
-        cancel_on_exit: true,
         threshold: 150,
-        // ... more custom settings?
       })
     </script>
   </body>

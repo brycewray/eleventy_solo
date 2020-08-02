252eed524605ef03ad84d055a3f7a98126f2b00b
Bryce Wray <bwray@wraytx.com>
2020-08-02 12:48:37 -0500

    Using Fathom URL to avoid... "X-Cache: Miss from CloudFront" problem

--- src/assets/utils/analytics.js
+++ src/assets/utils/analytics.js
@@ -4,7 +4,7 @@ var analyticsCode = ``
 metadata.domain == "brycewray.com"
   ? analyticsCode = `
   <!-- Fathom - beautiful, simple website analytics -->
-  <script src="https://boa.brycewray.com/script.js" site="CSERHYIA" excluded-domains="localhost" defer></script>
+  <script src="https://cdn.usefathom.com/script.js" site="CSERHYIA" excluded-domains="localhost" defer></script>
   <!-- / Fathom -->
   `
   : analyticsCode = ``

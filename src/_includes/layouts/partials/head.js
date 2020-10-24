cfed6b1eb25a4a54c07410e7c34bbba0846128e1
Bryce Wray <bwray@wraytx.com>
2020-10-24 15:37:06 -0500

    Removing now-unneeded preconnects

--- src/_includes/layouts/partials/head.js
+++ src/_includes/layouts/partials/head.js
@@ -109,8 +109,6 @@ module.exports = function(eleventyConfig) {
 
     <link rel="preconnect" href="https://res.cloudinary.com" />
 
-    <link rel="preload" href="/assets/fonts/public-sans-20200809-vf-roman-latin.woff2" as="font" type="font/woff2" crossorigin="anonymous" />
-    <link rel="preload" href="/assets/fonts/public-sans-20200809-vf-italic-latin.woff2" as="font" type="font/woff2" crossorigin="anonymous" />
     <link rel="preload" as="style" href="/css/index.css" />
     <link rel="stylesheet" href="/css/index.css" type="text/css" />
     <style>@-moz-document url-prefix() {.lazy:-moz-loading {visibility:hidden;}}.ieOnly {display: none;}@media all and (-ms-high-contrast: none), (-ms-high-contrast: active) {.ieOnly {display: block;}.notInIE{display: none;}}</style>

b341bb84e8e13bf13b06e8eb379eab9abd7a6a20
Bryce Wray <bwray@wraytx.com>
2020-11-09 15:07:13 -0600

    Adding `postcss-hash`

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,11 @@
+
 module.exports = {
   plugins: [
+    require('postcss-hash')({
+      algorithm: "sha512",
+      trim: 20,
+      manifest: './_data/manifest.json'
+    }),
     require('postcss-import'),
     require('tailwindcss'),
     require('postcss-preset-env')({ stage: 1 }),

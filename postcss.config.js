58042f2ebf0737ee4d213168f09024d91a21bc26
Bryce Wray <bwray@wraytx.com>
2020-11-10 06:58:23 -0600

    Hyphen, not dot, in CSS filename

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,12 @@
+const path = require('path')
 
 module.exports = {
   plugins: [
     require('postcss-hash')({
-      algorithm: "sha512",
+      // algorithm: "sha512", // default = "md5"
       trim: 20,
-      manifest: './_data/manifest.json'
+      manifest: './_data/manifest.json',
+      name: ({dir, name, hash, ext}) => path.join(dir, name + '-' + hash + ext)
     }),
     require('postcss-import'),
     require('tailwindcss'),

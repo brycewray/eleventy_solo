c1cb6ab5955634755df0f5435b3df57113abc554
Bryce Wray <bwray@wraytx.com>
2020-11-29 14:29:51 -0600

    Revert to TWCSS font defaults

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,11 +29,6 @@ module.exports = {
         '800': '#000088',
         '900': '#000066',
       },
-    },
-    fontFamily: {
-      'sans': ['ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', '"Noto Sans"', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
-      'serif': ['Georgia', 'ui-serif', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
-      'mono': ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
     }
   },
   variants: {},

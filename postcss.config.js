62e714a5788786d76a3acea73d81b3ce02d4dd7e
Bryce Wray <bwray@wraytx.com>
2020-12-08 09:48:08 -0600

    Further config chg re hashing

--- postcss.config.js
+++ postcss.config.js
@@ -4,13 +4,13 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('tailwindcss'),
-    require('postcss-preset-env')({ stage: 1 }),
-    require('postcss-clean'),
     require('postcss-hash')({
       // algorithm: "sha512", // default = "md5"
       trim: 20,
       manifest: './_data/manifest.json',
       name: ({dir, name, hash, ext}) => path.join(dir, name + '-' + hash + ext)
     }),
+    require('postcss-preset-env')({ stage: 1 }),
+    require('postcss-clean'),
   ],
 }

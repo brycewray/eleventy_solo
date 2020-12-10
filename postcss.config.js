f159e3d7863d0f02dcc9419a9574803c191d31b9
Bryce Wray <bwray@wraytx.com>
2020-12-10 16:12:13 -0600

    Removing postcss-hash

--- postcss.config.js
+++ postcss.config.js
@@ -5,14 +5,6 @@ module.exports = {
     require('postcss-import'),
     require('tailwindcss'),
     require('postcss-preset-env')({ stage: 1 }),
-    /*
-    require('postcss-hash')({
-      // algorithm: "sha512", // default = "md5"
-      trim: 20,
-      manifest: './_data/manifest.json',
-      name: ({dir, name, hash, ext}) => path.join(dir, name + '-' + hash + ext)
-    }),
-    */
     require('postcss-clean'),
   ],
 }

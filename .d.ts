5d077c458fd751b3b37b573a1ab90295562cc288
Bryce Wray <bwray@wraytx.com>
2020-12-17 21:47:10 +0000

    Adding for dev purposes

---
+++ .d.ts
@@ -0,0 +1,11 @@
+declare module 'tailwindcss/colors';
+
+declare module 'eleventy-plugin-error-overlay';
+declare module '@11ty/eleventy-plugin-rss';
+declare module 'eleventy-plugin-svg-contents';
+
+declare module 'markdown-it-footnote';
+declare module 'markdown-it-attrs';
+declare module 'markdown-it-bracketed-spans';
+
+declare module 'glob-all';

6638bc50951a1e3466c686f6d615d961f8b67678
Bryce Wray <bwray@wraytx.com>
2021-03-17 09:34:56 -0500

    Config for TWCSS and JIT

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   plugins: [
+    require('postcss-nested'),
+    require('postcss-import'),
+    require('@tailwindcss/jit'),
     require('autoprefixer'),
   ],
 }
---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: {
+    content: [
+      './src/**/*.js',
+      './src/**/*.11ty.js',
+      './src/**/*.njk'
+    ],
+  },
+  darkMode: 'media',
+  theme: {
+    fontWeight: {
+      normal: 400,
+      bold: 625, // not default of 700
+      black: 900,
+    },
+    colors: {
+      transparent: 'transparent',
+      white: '#ffffff',
+      black: '#000000',
+      gray: colors.gray,
+      yellow: colors.yellow,
+      blue: {
+        '100': '#bbeeff',
+        '200': '#00aaff',
+        '300': '#0088ff',
+        '400': '#0033ff',
+        '500': '#0000ff',
+        '600': '#0000bb',
+        '700': '#0000aa',
+        '800': '#000088',
+        '900': '#000066',
+      },
+    },
+  },
+  variants: {},
+  plugins: [], // if we add forms, do it here
+}

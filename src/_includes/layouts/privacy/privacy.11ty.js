1090252df93a155b75698d18a43861d8de0e4f8e
Bryce Wray <bwray@wraytx.com>
2021-03-18 15:05:09 -0500

    Formatting of "billboard" area

--- src/_includes/layouts/privacy/privacy.11ty.js
+++ src/_includes/layouts/privacy/privacy.11ty.js
@@ -25,7 +25,7 @@ exports.render = function (data) {
       </span>
     </p>
   </div>
-  <div class="sm:w-5/6 md:w-4/5 xl:w-1/2 xb:w-5/12 mt-10 mr-auto ml-auto px-6 lg:px-16" style="max-width: 60ch;">
+  <div class="sm:w-5/6 md:w-4/5 xl:w-1/2 2xl:w-5/12 mt-8 mx-auto px-6 lg:px-0">
     <div class="border-t-2 border-blue-700 block mt-0 mx-auto mb-8 w-60"></div>
     <article id="articleContent">
       ${data.content}

642f35f6992649b4ea695808a1b1269d7fcde451
Bryce Wray <bwray@wraytx.com>
2021-03-18 10:16:22 -0500

    Tweaks

--- src/_includes/layouts/_default/index.11ty.js
+++ src/_includes/layouts/_default/index.11ty.js
@@ -6,7 +6,7 @@ exports.render = function (data) {
   return /*html*/ `
   <main class="p-0">
     <div class="pt-6 lg:pt-12 lg:flex w-full mr-auto ml-auto">
-      <div class="bg-transparent lg:bg-gradient-to-b from-blue-700 to-black lg:dark:bg-gradient-to-b lg:dark:from-black lg:dark:to-blue-700 lg:w-5/12 px-8 lg:pl-16 lg:pr-16 pt-16 lg:text-white text-center lg:text-right">
+      <div class="bg-transparent lg:bg-gradient-to-b from-blue-700 to-black lg:dark:bg-gradient-to-b lg:dark:from-black lg:dark:to-blue-700 lg:w-5/12 px-8 lg:px-12 pt-16 lg:text-white text-center lg:text-right">
         <h1 class="lg:leading-normal text-3xl lg:text-4xl 2xl:text-6xl lg:pt-10">Welcome to brycewray.com, offering <a href="/about" class="lg:text-blue-100 lg:border-blue-100">its owner</a>&rsquo;s &ldquo;observations, opinions, and geekiness&rdquo; since&nbsp;2018.</h1>
       </div>
       <div class="text-center lg:text-left lg:w-7/12 lg:pl-16 lg:pt-16">

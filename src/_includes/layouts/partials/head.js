a4c7b741302c292459a99eba733ded12468600ea
Bryce Wray <bwray@wraytx.com>
2021-03-21 15:31:41 -0500

    Order suggested by Lighthouse

--- src/_includes/layouts/partials/head.js
+++ src/_includes/layouts/partials/head.js
@@ -18,11 +18,10 @@ module.exports = function(eleventyConfig) {
     
     return /*html*/ `
   <head>
-    <meta http-equiv="X-UA-Compatible" content="IE=10"><!-- due to IE 11 issue with TWCSS -->
-
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=10"><!-- due to IE 11 issue with TWCSS -->
 
     <meta name="generator" content="Eleventy v${require(`@11ty/eleventy/package.json`).version}" />  
 

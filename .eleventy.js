eb82ab97daa7099eb9a33ef87a3279ef0d735221
Bryce Wray <bwray@wraytx.com>
2021-03-26 08:49:47 -0500

    Enabling and styling 404 page

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
+const fs = require("fs")
 const { DateTime } = require("luxon")
 const htmlmin = require("html-minifier")
 const ErrorOverlay = require("eleventy-plugin-error-overlay")
@@ -112,6 +113,18 @@ module.exports = function (eleventyConfig) {
     files: ["src/**/*.js", "src/assets/css/*.css", "src/**/*.md"],
     ghostMode: false,
     port: 3000,
+    callbacks: {
+      ready: function(err, bs) {
+        bs.addMiddleware("*", (req, res) => {
+          const content_404 = fs.readFileSync('_site/404.html')
+          // Add 404 http status code in request header.
+          res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" })
+          // Provides the 404 content without redirect.
+          res.write(content_404)
+          res.end()
+        })
+      }
+    }
   })
 
   eleventyConfig.addPlugin(ErrorOverlay)

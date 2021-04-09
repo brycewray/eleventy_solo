a8c7f33b61c1e2bd04fe29d228722f932bb3ff93
Bryce Wray <bwray@wraytx.com>
2021-04-09 04:44:52 -0500

    Styling tweaks

--- src/_includes/layouts/posts/singlepost.11ty.js
+++ src/_includes/layouts/posts/singlepost.11ty.js
@@ -46,14 +46,14 @@ exports.render = function (data) {
       <div class="w-full px-8 md:px-0 bg-blue-700 align-middle mt-10 mb-10">
         <h3 class="text-center text-3xl tracking-normal mb-2 pt-2"><a href="/posts" class="border-transparent text-blue-100 hover:text-white">Other posts</a></h3>
         ${data.nextPost && data.nextPost.url !== null
-          ? /*html*/ `<p class="text-center my-2 text-xl text-white leading-tight tracking-tight">
+          ? /*html*/ `<p class="text-center my-2 text-xl text-white leading-tight">
             <strong>Next</strong>: 
             <a class="border-transparent text-blue-100 hover:text-white hover:border-blue-100" href="${data.nextPost.url}">${data.nextPost.data.title}</a>
           </p>`
           : ``
         }
       ${data.prevPost && data.prevPost.url !== null
-        ? /*html*/ `<p class="text-center pb-4 my-2 text-xl text-white leading-tight tracking-tight">
+        ? /*html*/ `<p class="text-center pb-4 my-2 text-xl text-white leading-tight">
           <strong>Previous</strong>: 
           <a class="border-transparent text-blue-100 hover:text-white hover:border-blue-100" href="${data.prevPost.url}">${data.prevPost.data.title}</a>
         </p>`

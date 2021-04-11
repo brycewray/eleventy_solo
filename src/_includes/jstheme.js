a8a9fbbda17b1b9b3f58411bd9e1a49c4ab01bca
Bryce Wray <bwray@wraytx.com>
2021-04-11 10:33:17 -0500

    Simpler style-editing in .11ty.js

--- src/_includes/jstheme.js
+++ src/_includes/jstheme.js
@@ -4,14 +4,14 @@
 var headTag = require('./layouts/partials/head') // head.js
 var siteHeader = require('./layouts/partials/header') // etc. ...
 var siteFooter = require('./layouts/partials/footer')
-var billBoard = require('./layouts/partials/billboard')
+// var billBoard = require('./layouts/partials/billboard')
 
 module.exports = function(eleventyConfig) {
 
   headTag(eleventyConfig)
   siteHeader(eleventyConfig)
   siteFooter(eleventyConfig)
-  billBoard(eleventyConfig)
+  // billBoard(eleventyConfig)
 
   return
   
--- src/_includes/layouts/posts/singlepost.11ty.js
+++ src/_includes/layouts/posts/singlepost.11ty.js
@@ -7,7 +7,26 @@ exports.data = {
 exports.render = function (data) {
   return /*html*/ `
 <main class="pt-12">
-  ${this.billBoard(data)}  
+  <div class="text-center px-4 md:px-6 lg:px-8 2xl:px-12">
+    <h1 class="tracking-tight text-4xl md:text-5xl lg:text-7xl pt-8 lg:pt-16">${data.title}</h1>      
+    ${
+      data.subtitle
+        ? /*html*/ `
+        <h2 class="mt-6 lg:mt-8 2xl:mt-12 text-2xl lg:text-4xl 2xl:text-5xl">
+          ${data.subtitle}
+        </h2>
+        `
+        : ``
+    }
+    <p class="mt-8 lg:mt-12 2xl:mt-16 text-xl lg:text-2xl 2xl:text-3xl">${data.description}</p>
+    <p class="text-lg lg:text-xl mt-4 lg:mt-6 2xl:mt-8"><strong>${this.pub_lastmod(data.page.date)}</strong>
+      ${
+        data.lastmod !== null && data.lastmod !== undefined
+        ? /*html*/ `<br /><span class="text-base">Last modified ${this.pub_lastmod(data.lastmod)}</span>`
+        : ``
+      }
+    </p>
+  </div>
   <div class="sm:w-5/6 md:w-4/5 xl:w-1/2 2xl:w-5/12 mt-8 mx-auto px-6 lg:px-0">
     ${stringtoRet(data.featured_image, data.featured_image_alt, data.featured_image_width, data.featured_image_height)}
     <p class="mt-2 mb-6 md:mb-8 lg:mb-12 text-center text-xs">

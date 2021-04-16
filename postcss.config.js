73f5650b98b43a9f278316148262cc35ae8af866
Bryce Wray <bwray@wraytx.com>
2021-04-15 20:12:46 -0500

    Remove purgecss (makes little diff)

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,5 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: [
-    './**/*.njk',
-    './**/*.11ty.js',
-    './**/*.html'
-  ]
-})
-
 module.exports = {
   plugins: [
     require('autoprefixer'),
-    ...process.env.NODE_ENV === 'production'
-      ? [purgecss]
-      : []
   ],
 }

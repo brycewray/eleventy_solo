a5a11ffc0381ec06847557c0c604eb8475d98d91
Bryce Wray <bwray@wraytx.com>
2021-04-28 07:09:35 -0500

    Restructuring

--- src/_includes/layouts/posts/singlepost.11ty.js
+++ src/_includes/layouts/posts/singlepost.11ty.js
@@ -4,39 +4,44 @@ exports.data = {
   layout: 'base'
 }
 
-exports.render = function (data) {
+exports.render = function (data) {  // restructuring for easier reading/typing...
+  // https://wesbos.com/destructuring-objects
+  // https://stackoverflow.com/questions/65565806/destructure-object-properties-inside-array-prototype-map
+
+  const { featured_image, featured_image_alt, featured_image_caption, featured_image_width, featured_image_height, content, page, prevPost, nextPost } = data
+
   return /*html*/ `
   <main>
     ${this.billBoard(data)}
 
     <div class="container-narrower">
-      ${stringtoRet(data.featured_image, data.featured_image_alt, data.featured_image_width, data.featured_image_height)}
+      ${stringtoRet(featured_image, featured_image_alt, featured_image_width, featured_image_height)}
       <p class="ctr legal">
-        ${ data.featured_image_caption 
-          ? `${data.featured_image_caption}`
+        ${ featured_image_caption 
+          ? `${featured_image_caption}`
           : /*html*/ `&nbsp;`
         }
       </p>
       <article class="article">
-        ${data.content}
+        ${content}
       </article>
     </div>
     
-    ${data.page.url !== "/about/"
+    ${page.url !== "/about/"
     ? /*html*/ `
       <div class="bg-dark">
         <h3 class="ctr wht prevnextH3"><a href="/posts" class="border-transparent text-blue-100 hover:text-white">Other posts</a></h3>
-        ${data.nextPost && data.nextPost.url !== null
+        ${nextPost && nextPost.url !== null
           ? /*html*/ `<p class="ctr prevnextP">
             <strong>Next</strong>: 
-            <a class="next" href="${data.nextPost.url}">${data.nextPost.data.title}</a>
+            <a class="next" href="${nextPost.url}">${nextPost.data.title}</a>
           </p>`
           : ``
         }
-        ${data.prevPost && data.prevPost.url !== null
+        ${prevPost && prevPost.url !== null
           ? /*html*/ `<p class="ctr prevnextP">
             <strong>Previous</strong>: 
-            <a class="previous" href="${data.prevPost.url}">${data.prevPost.data.title}</a>
+            <a class="previous" href="${prevPost.url}">${prevPost.data.title}</a>
           </p>`
           : ``
         }

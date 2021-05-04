0a6b2bd9b64360e354ae0e066f7816d485b280c5
Bryce Wray <bwray@wraytx.com>
2021-05-04 14:52:52 -0500

    Cleanup of OG and Twitter items

--- src/_includes/layouts/partials/head.js
+++ src/_includes/layouts/partials/head.js
@@ -1,8 +1,9 @@
-const fs = require('fs')
 const alpineJSVer = '2.8.2'
+const fs = require('fs')
 let socialImg = `https://res.cloudinary.com/brycewray-com/image/upload/`
 socialImg += `c_fill,w_1024,h_512,q_auto:eco,f_auto,x_0,z_1/`
 const fallbackImg = `typewriter-monochrome_2242164_6260x4374.jpg`
+const fallbackImg_Alt = `Monochrome photo of hands typing on a typewriter`
 const analyticsCode = require('../../../assets/utils/analytics.js')
 var internalCSS = ''
 var internalCSSPath = 'src/_includes/css/index.css'
@@ -16,16 +17,16 @@ module.exports = function(eleventyConfig) {
 
   // restructuring for easier reading/typing
   // ... https://wesbos.com/destructuring-objects
-  eleventyConfig.addShortcode('headTag', function({ siteparams, page, description, title, featured_image }) {
+  eleventyConfig.addShortcode('headTag', function({ siteparams, page, description, title, featured_image, featured_image_alt }) {
     const { siteTitle, siteDescription, siteURLforOG } = siteparams
     const { url } = page
 
     return /*html*/ `
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
 
     <meta name="generator" content="Eleventy v${require(`@11ty/eleventy/package.json`).version}" />  
 
@@ -42,8 +43,21 @@ module.exports = function(eleventyConfig) {
       <link rel="canonical" href="${siteURLforOG + url}" />
       `
     }
+
+    <meta property="og:type" content="website" />
     
-    <meta property="og:image" content="${socialImg + fallbackImg}" />
+    <meta property="og:image" content="${featured_image
+      ? `${socialImg + featured_image}`
+      : `${socialImg + fallbackImg}`
+    }" />
+
+    <meta property="og:image:alt" content="${featured_image_alt
+      ? `${featured_image_alt}`
+      : `${fallbackImg_Alt}`
+    }" />
+
+    <meta property="og:image:width" content="1024" />
+    <meta property="og:image:height" content="512" />
 
     <meta name="description" content="${description
       ? `${description}`
@@ -56,7 +70,7 @@ module.exports = function(eleventyConfig) {
     }" />
 
     <meta property="og:url" content="${url
-      ? `${url}`
+      ? `${siteURLforOG + url}`
       : `${siteURLforOG}`
     }" />
 
@@ -64,7 +78,10 @@ module.exports = function(eleventyConfig) {
     <meta name="twitter:site" content="@BryceWrayTX">
     <meta name="twitter:creator" content="@BryceWrayTX">
     <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:image" content="${socialImg + fallbackImg}" />
+    <meta name="twitter:image" content="${featured_image
+      ? `${socialImg + featured_image}`
+      : `${socialImg + fallbackImg}`
+    }" />
 
     <meta name="twitter:description" content="${description
       ? `${description}`
@@ -73,7 +90,7 @@ module.exports = function(eleventyConfig) {
 
     <meta name="twitter:title" content="${title
       ? `${title}`
-      : `${siteTitle}`   
+      : `${siteTitle}`
     }" />
 
 
@@ -81,28 +98,28 @@ module.exports = function(eleventyConfig) {
 
     <!-- generics -->
     <link rel="icon" type="image/svg+xml" href="/assets/svg/svgNavIcon_reduced.svg" />
-    <link rel="icon" type="image/png" href="/images/icons/favicon-16x16.png" sizes="16x16">
+    <link rel="icon" type="image/png" href="/images/icons/favicon-16x16.png" sizes="16x16" />
     <!--
-    <link rel="icon" type="image/png" href="/images/icons/favicon-32x32.png" sizes="32x32">
-    <link rel="icon" type="image/png" href="/images/icons/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="/images/icons/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="/images/icons/favicon-96x96.png" sizes="96x96" />
     -->
 
     <!-- iOS -->
-    <link rel="apple-touch-icon" sizes="120x120" href="/images/icons/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="/images/icons/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="167x167" href="/images/icons/apple-touch-icon-167x167.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="/images/icons/apple-icon-180x180.png">
-    <link rel="apple-touch-icon" sizes="1024x1024" href="/images/icons/apple-icon-1024x1024.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/images/icons/apple-icon-120x120.png" />
+    <link rel="apple-touch-icon" sizes="152x152" href="/images/icons/apple-icon-152x152.png" />
+    <link rel="apple-touch-icon" sizes="167x167" href="/images/icons/apple-touch-icon-167x167.png" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/icons/apple-icon-180x180.png" />
+    <link rel="apple-touch-icon" sizes="1024x1024" href="/images/icons/apple-icon-1024x1024.png" />
 
     <!-- Android -->
-    <link rel="shortcut icon" sizes="196x196" href="/images/icons/favicon-196x196.png">
+    <link rel="shortcut icon" sizes="196x196" href="/images/icons/favicon-196x196.png" />
 
     <!-- Windows 8, IE 10 -->
-    <meta name="msapplication-TileColor" content="#FFFFFF">
-    <meta name="msapplication-TileImage" content="/images/ms-icon-144x144.png">
+    <meta name="msapplication-TileColor" content="#FFFFFF" />
+    <meta name="msapplication-TileImage" content="/images/ms-icon-144x144.png" />
 
     <!-- Windows 8.1 and up, IE 11 -->
-    <meta name="msapplication-config" content="/browserconfig.xml">
+    <meta name="msapplication-config" content="/browserconfig.xml" />
 
     <!-- **** CONCLUSION, favicons **** -->
 

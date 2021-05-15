1db478194a7f0c02012668ac93d1e7494acdbda5
Bryce Wray <bwray@wraytx.com>
2021-05-15 18:11:07 -0500

    ...and they're back

--- src/assets/utils/twitscrn.js
+++ src/assets/utils/twitscrn.js
@@ -5,19 +5,21 @@ This shortcode takes the following form...
 
 const respSizes = require(`../../../_data/siteparams.json`).respSizes
 var cloudiBase = 'https://res.cloudinary.com/brycewray-com/image/upload/'
-// var LQIPholder = 'f_auto,q_1,w_20/' // note ending slash
+var LQIPholder = 'f_auto,q_1,w_20/' // note ending slash
 var xFmPart1 = 'f_auto,q_auto:best,w_'
 var xFmPart2 = ',x_0,z_1/' // note ending slash
  
 module.exports = (imageUrl, alt, width, height, twitterUrl) => {
-  imgClass = `containedImage twitter-tweet`
+  divClass = `twitter-tweet`
+  imgClass = `containedImage lazy`
   nscClass = `containedImage`
   dataSzes = `(min-width: 1024px) 100vw, 50vw`
   
   var separator = ', '
 
   var stringtoRet = ``
-  stringtoRet = `<a href="${twitterUrl}" target="_blank" rel="noopener"><img class="${imgClass}" aspect-ratio="${width} / ${height}" src="${cloudiBase + xFmPart1 + "600" + xFmPart2 + imageUrl}" srcset="`
+  stringtoRet = `<div class="${divClass}" style="aspect ratio: ${width} / ${height};">
+  <a href="${twitterUrl}" target="_blank" rel="noopener"><img class="${imgClass}" data-src="${cloudiBase + xFmPart1 + "600" + xFmPart2 + imageUrl}" data-srcset="`
   respSizes.forEach(size => {
     if (size <= width) {
       stringtoRet += `${cloudiBase + xFmPart1 + size + xFmPart2 + imageUrl} ${size}w`
@@ -25,10 +27,11 @@ module.exports = (imageUrl, alt, width, height, twitterUrl) => {
     }
   })
   stringtoRet = stringtoRet.substring(0, stringtoRet.length - 2)
-  stringtoRet += `" alt="${alt}" width="${width}" height="${height}" loading="lazy" sizes="${dataSzes}" /></a>
+  stringtoRet += `" alt="${alt}" width="${width}" height="${height}" loading="lazy" data-sizes="${dataSzes}" /></a>
   <noscript>
     <a href="${twitterUrl}" target="_blank" rel="noopener"><img class="${nscClass}" src="${cloudiBase + xFmPart1 + "300" + xFmPart2 + imageUrl}" alt="${alt}" loading="lazy" /></a>
-  </noscript>`
+  </noscript>
+  </div>`
 
   return stringtoRet
 }

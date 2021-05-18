0216c6cab47371fbc649c534b904aa3b7efd9aa0
Bryce Wray <bwray@wraytx.com>
2021-05-18 08:22:19 -0500

    Removing Volument

--- src/assets/utils/analytics.js
+++ src/assets/utils/analytics.js
@@ -5,18 +5,7 @@ metadata.domain == "brycewray.com"
   ? analyticsCode = /*HTML*/`
   <!-- Fathom - beautiful, simple website analytics -->
   <script src="https://boa.brycewray.com/script.js" site="CSERHYIA" included-domains="brycewray.com,www.brycewray.com" defer></script>
-  <!-- / Fathom -->
-
-  <!-- Analytics by volument.com -->
-  <script>
-    function startVolument() {
-      volument.start({
-        token: 'c63cb179db',
-        policy_url: '/privacy',
-      })
-    }
-  </script>
-  <script async src="https://cdn.volument.com/v1/volument-ui.js" onload="startVolument()"></script>  
+  <!-- / Fathom --> 
   `
   : analyticsCode = ``
 

eb290b029366b3de11c68f8dbf0bf46ccbf40ac2
Bryce Wray <bwray@wraytx.com>
2021-05-18 07:30:52 -0500

    More re Volument

--- src/assets/utils/analytics.js
+++ src/assets/utils/analytics.js
@@ -2,13 +2,21 @@ const metadata = require('../../../_data/metadata.json')
 
 var analyticsCode = ``
 metadata.domain == "brycewray.com"
-  ? analyticsCode = `
+  ? analyticsCode = /*HTML*/`
   <!-- Fathom - beautiful, simple website analytics -->
   <script src="https://boa.brycewray.com/script.js" site="CSERHYIA" included-domains="brycewray.com,www.brycewray.com" defer></script>
   <!-- / Fathom -->
 
   <!-- Analytics by volument.com -->
-  <script async src="https://cdn.volument.com/v1/volument-ui.js" onload="volument('c63cb179db')"></script>  
+  <script>
+    function startVolument() {
+      volument.start({
+        token: 'c63cb179db',
+        policy_url: '/privacy',
+      })
+    }
+  </script>
+  <script async src="https://cdn.volument.com/v1/volument-ui.js" onload="startVolument()"></script>  
   `
   : analyticsCode = ``
 

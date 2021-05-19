b307a10f6fa404c783495e6875bf10c1c5aeabe6
Bryce Wray <bwray@wraytx.com>
2021-05-19 14:39:22 -0500

    Taking out featured images again

--- src/_includes/layouts/posts/singlepost.11ty.js
+++ src/_includes/layouts/posts/singlepost.11ty.js
@@ -8,21 +8,15 @@ exports.render = function (data) {  // restructuring for easier reading/typing..
   // https://wesbos.com/destructuring-objects
   // https://stackoverflow.com/questions/65565806/destructure-object-properties-inside-array-prototype-map
 
-  const { featured_image, featured_image_alt, featured_image_caption, featured_image_width, featured_image_height, content, page, prevPost, nextPost } = data
+  const { content, page, prevPost, nextPost } = data
 
   return /*html*/ `
   <main>
     ${this.billBoard(data)}
 
     <div class="container-narrower">
-    ${stringtoRet(data.featured_image, data.featured_image_alt, data.featured_image_width, data.featured_image_height)}
-    <p class="ctr legal">
-      ${ data.featured_image_caption 
-        ? `${data.featured_image_caption}`
-        : /*html*/ `&nbsp;`
-      }
-    </p>
-    <article class="article">
+      <div class="post-line"></div>
+      <article class="article">
         ${content}
       </article>
     </div>

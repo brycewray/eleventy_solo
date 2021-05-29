e82bba986e5e627bca412179f29958eecaa14cfc
Bryce Wray <bwray@wraytx.com>
2021-05-29 07:29:35 -0500

    Back to no-VLL because of CLS on Safari

--- src/assets/utils/imgc.js
+++ src/assets/utils/imgc.js
@@ -3,46 +3,23 @@ This shortcode takes the following form...
   {% imgc url, alt, width, height %}
 ...with url in the form of (note NO leading or ending slash):
   filename.ext
-...and 'temp[late]' optional in body copy. The template is used 
-to specify hero images on either the home page ('index') or 
-post pages ('posts'). Without this parameter, the `switch` 
-statement below defaults to body copy-style image-handling.
 */
 
 const respSizes = require(`../../../_data/siteparams.json`).respSizes
 var cloudiBase = 'https://res.cloudinary.com/brycewray-com/image/upload/'
-var LQIPholder = 'f_auto,q_1,w_20/' // note ending slash
+// var LQIPpholder = 'f_auto,q_1,w_20/' // note ending slash
 var xFmPart1 = 'f_auto,q_auto:eco,w_'
 var xFmPart2 = ',x_0,z_1/' // note ending slash
  
-module.exports = (url, alt, width, height, tmpl) => {
-  if (!tmpl) tmpl == "none"
-
-  switch(tmpl) {
-    case 'index':
-      divClass = `h-full`
-      imgClass = `containedImage lazy`
-      nscClass = `object-cover object-center h-full w-full containedImage`
-      dataSzes = `100vw`
-      break
-    case 'posts':
-      divClass = `h-full`
-      imgClass = `containedImage lazy`
-      nscClass = `imgCover hero`
-      dataSzes = `100vw`
-      break
-    default:
-      divClass = `relative`
-      imgClass = `containedImage lazy`
-      nscClass = `containedImage`
-      dataSzes = `(min-width: 1024px) 100vw, 50vw`
-  }
+module.exports = (url, alt, width, height) => {
+  imgClass = `containedImage`
+  nscClass = `containedImage`
+  dataSzes = `(min-width: 1024px) 100vw, 50vw`
   
   var separator = ', '
 
   var stringtoRet = ``
-  stringtoRet = `<div class="${divClass}" style="aspect ratio: ${width} / ${height}; background-image: url(${cloudiBase + LQIPholder + url}); background-position: center; background-repeat: no-repeat; background-size: cover;">
-  <img class="${imgClass}" data-src="${cloudiBase + xFmPart1 + "600" + xFmPart2 + url}" data-srcset="`
+  stringtoRet = `<img class="${imgClass}" aspect-ratio="${width} / ${height}" src="${cloudiBase + xFmPart1 + "600" + xFmPart2 + url}" srcset="`
   respSizes.forEach(size => {
     if (size <= width) {
       stringtoRet += `${cloudiBase + xFmPart1 + size + xFmPart2 + url} ${size}w`
@@ -50,11 +27,12 @@ module.exports = (url, alt, width, height, tmpl) => {
     }
   })
   stringtoRet = stringtoRet.substring(0, stringtoRet.length - 2)
-  stringtoRet += `" alt="${alt}" width="${width}" height="${height}" loading="lazy" data-sizes="${dataSzes}" />
+  stringtoRet += `" alt="${alt}" width="${width}" height="${height}"`
+  stringtoRet += ` loading="lazy"` // not good for above-the-fold images
+  stringtoRet +=` sizes="${dataSzes}" />
   <noscript>
-    <img class="${nscClass}" src="${cloudiBase + xFmPart1 + "300" + xFmPart2 + url}" alt="${alt}" loading="lazy" />
-  </noscript>
-  </div>`
+    <img class="${nscClass}" src="${cloudiBase + xFmPart1 + "300" + xFmPart2 + url}" alt="${alt}" />
+  </noscript>`
 
   return stringtoRet
 }

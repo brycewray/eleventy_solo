2a347b383d50c7da1590cbf6959f95e48fc83a3a
Bryce Wray <bwray@wraytx.com>
2021-07-05 08:13:31 -0500

    Move Segoe UI farther back

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,7 +36,7 @@ module.exports = {
       },
     },
     fontFamily: {
-      'sans': ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji',],
+      'sans': ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Roboto', 'Helvetica Neue', 'Helvetica', 'Arial', 'Noto Sans', 'Segoe UI', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji',],
     },
   },
   variants: {},

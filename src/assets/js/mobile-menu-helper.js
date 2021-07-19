5f62db646274f588b3b13cbef0dba9084674fb4b
Bryce Wray <bwray@wraytx.com>
2021-07-19 10:11:08 -0500

    Adjusting JS and event-handling for CSP

--- src/assets/js/mobile-menu-helper.js
+++ src/assets/js/mobile-menu-helper.js
@@ -1,6 +1,6 @@
 // based on https://www.creative-tim.com/learning-lab/tailwind-starter-kit/documentation/javascript/navbars
 
-function toggleNavbar(collapseID){
-  document.getElementById(collapseID).classList.toggle("hidden");
-  document.getElementById(collapseID).classList.toggle("flex");
-}
+document.getElementById('collapse-button').addEventListener("click", function() {
+  document.getElementById('collapse-navbar').classList.toggle("hidden")
+  document.getElementById('collapse-navbar').classList.toggle("flex")
+})

b70f07ee84aea27d802f47ff71ea0414a993d8f7
Bryce Wray <bwray@wraytx.com>
2021-07-19 07:10:40 -0500

    Respect :-)

--- src/assets/js/mobile-menu-helper.js
+++ src/assets/js/mobile-menu-helper.js
@@ -1,3 +1,5 @@
+// based on https://www.creative-tim.com/learning-lab/tailwind-starter-kit/documentation/javascript/navbars
+
 function toggleNavbar(collapseID){
   document.getElementById(collapseID).classList.toggle("hidden");
   document.getElementById(collapseID).classList.toggle("flex");
